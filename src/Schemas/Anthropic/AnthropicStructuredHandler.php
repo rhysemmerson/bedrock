63ea4bd8a61a9e82296c66404a23dc00ad8f93aa
Chris <101201552+ChrisB-TL@users.noreply.github.com>
2025-04-23 17:49:15 +0100

    feat(anthropic): improved structured prompt (#3)

--- src/Schemas/Anthropic/AnthropicStructuredHandler.php
+++ src/Schemas/Anthropic/AnthropicStructuredHandler.php
@@ -120,7 +120,7 @@ class AnthropicStructuredHandler extends BedrockStructuredHandler
     protected function appendMessageForJsonMode(Request $request): void
     {
         $request->addMessage(new UserMessage(sprintf(
-            "Respond with ONLY JSON that matches the following schema: \n %s",
+            "Respond with ONLY JSON (i.e. not in backticks or a code block, with NO CONTENT outside the JSON) that matches the following schema: \n %s",
             json_encode($request->schema()->toArray(), JSON_PRETTY_PRINT)
         )));
     }
