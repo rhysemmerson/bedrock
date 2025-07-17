97d4ff118cfe5d30022e8d3b248d097afa7b0508
Rhys Emmerson <rhys@iseekplant.com.au>
2025-07-18 09:19:07 +1000

    Add test for usage, capture cache usage

--- tests/Schemas/Converse/ConverseStreamHandlerTest.php
+++ tests/Schemas/Converse/ConverseStreamHandlerTest.php
@@ -10,13 +10,16 @@ use Prism\Bedrock\Enums\BedrockSchema;
 use Prism\Prism\Enums\FinishReason;
 use Prism\Prism\Facades\Tool;
 use Prism\Prism\Prism;
+use Prism\Prism\ValueObjects\Messages\AssistantMessage;
+use Prism\Prism\ValueObjects\Messages\SystemMessage;
+use Prism\Prism\ValueObjects\Messages\UserMessage;
 use Tests\Fixtures\FixtureResponse;
 
 it('streams output', function (): void {
     FixtureResponse::fakeStreamResponses('converse-stream', 'converse/stream-basic-text');
 
     $response = Prism::text()
-        ->using('bedrock', 'apac.anthropic.claude-3-5-sonnet-20240620-v1:0')
+        ->using('bedrock', 'us.amazon.nova-micro-v1:0')
         ->withProviderOptions(['apiSchema' => BedrockSchema::Converse])
         ->withPrompt('Who are you?')
         ->asStream();
@@ -36,11 +39,6 @@ it('streams output', function (): void {
 
     expect($finalChunk->finishReason)->toBe(FinishReason::Stop);
 
-    expect($finalChunk->usage->promptTokens)->toBe(11);
-    expect($finalChunk->usage->completionTokens)->toBe(60);
-    expect($finalChunk->usage->cacheWriteInputTokens)->toBeNull();
-    expect($finalChunk->usage->cacheReadInputTokens)->toBeNull();
-
     // Verify the HTTP request
     Http::assertSent(fn (Request $request): bool => str_ends_with($request->url(), 'converse-stream'));
 
@@ -50,40 +48,87 @@ it('streams output', function (): void {
             'model trained to engage in conversation and help with tasks. How can I assist you today?');
 });
 
-it('can handle tool calls', function (): void {
-    FixtureResponse::fakeStreamResponses('converse-stream', 'converse/stream-handle-tool-cals');
+it('can return usage with a basic stream', function (): void {
+    FixtureResponse::fakeStreamResponses('converse-stream', 'converse/stream-basic-text-with-cache-usage');
 
-    $tools = [
-        Tool::as('weather')
-            ->for('useful when you need to search for current weather conditions')
-            ->withStringParameter('city', 'The city that you want the weather for')
-            ->using(fn (string $city): string => 'The weather will be 75° and sunny'),
-        Tool::as('search')
-            ->for('useful for searching curret events or data')
-            ->withStringParameter('query', 'The detailed search query')
-            ->using(fn (string $query): string => 'The tigers game is at 3pm in detroit'),
-    ];
+    // Read a large system prompt from cache
+    // Write a conversation to cache
 
     $response = Prism::text()
-        ->using('bedrock', 'apac.anthropic.claude-3-5-sonnet-20240620-v1:0')
-        ->withProviderOptions(['apiSchema' => BedrockSchema::Converse])
-        ->withPrompt('What is the weather like in Detroit today?')
-        ->withMaxSteps(2)
-        ->withTools($tools)
+        ->using('bedrock', 'us.amazon.nova-micro-v1:0')
+        ->withSystemPrompt(
+            (new SystemMessage(
+                collect(range(1, 1000))
+                    ->map(fn ($i): string|false => \NumberFormatter::create('en', \NumberFormatter::SPELLOUT)->format($i))
+                    ->implode(' ')
+            ))
+                ->withProviderOptions(['cacheType' => 'default'])
+        )
+        ->withMessages([
+            new UserMessage('Who are you?'),
+            (new AssistantMessage('Hi I\'m Nova'))
+                ->withProviderOptions(['cacheType' => 'default']),
+            new UserMessage('Nice to meet you Nova'),
+        ])
         ->asStream();
 
-    $toolCalls = [];
-    $toolResults = [];
+    $text = '';
+    $chunks = [];
 
     foreach ($response as $chunk) {
-        if ($chunk->toolCalls !== []) {
-            $toolCalls[] = $chunk->toolCalls;
-        }
-        if ($chunk->toolResults !== []) {
-            $toolResults[] = $chunk->toolResults;
-        }
+        $chunks[] = $chunk;
+        $text .= $chunk->text;
     }
 
-    expect($toolCalls)->not()->toBeEmpty();
-    expect($toolResults)->not()->toBeEmpty();
+    expect((array) end($chunks)->usage)->toBe([
+        'promptTokens' => 67,
+        'completionTokens' => 48,
+        'cacheWriteInputTokens' => 131,
+        'cacheReadInputTokens' => 4230,
+        'thoughtTokens' => null,
+    ]);
+
+    // Verify the HTTP request
+    Http::assertSent(fn (Request $request): bool => str_ends_with($request->url(), 'converse-stream'));
+});
+
+describe('tools', function (): void {
+
+    it('can handle tool calls', function (): void {
+        FixtureResponse::fakeStreamResponses('converse-stream', 'converse/stream-handle-tool-cals');
+
+        $tools = [
+            Tool::as('weather')
+                ->for('useful when you need to search for current weather conditions')
+                ->withStringParameter('city', 'The city that you want the weather for')
+                ->using(fn (string $city): string => 'The weather will be 75° and sunny'),
+            Tool::as('search')
+                ->for('useful for searching curret events or data')
+                ->withStringParameter('query', 'The detailed search query')
+                ->using(fn (string $query): string => 'The tigers game is at 3pm in detroit'),
+        ];
+
+        $response = Prism::text()
+            ->using('bedrock', 'us.amazon.nova-micro-v1:0')
+            ->withProviderOptions(['apiSchema' => BedrockSchema::Converse])
+            ->withPrompt('What is the weather like in Detroit today?')
+            ->withMaxSteps(2)
+            ->withTools($tools)
+            ->asStream();
+
+        $toolCalls = [];
+        $toolResults = [];
+
+        foreach ($response as $chunk) {
+            if ($chunk->toolCalls !== []) {
+                $toolCalls[] = $chunk->toolCalls;
+            }
+            if ($chunk->toolResults !== []) {
+                $toolResults[] = $chunk->toolResults;
+            }
+        }
+
+        expect($toolCalls)->not()->toBeEmpty();
+        expect($toolResults)->not()->toBeEmpty();
+    });
 });
