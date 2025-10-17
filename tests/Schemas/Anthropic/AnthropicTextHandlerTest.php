9e7df1f3f305c957293146a013667f11a43ca3c9
Rhys Emmerson <rhys@iseekplant.com.au>
2025-10-17 14:34:38 +1000

    cleanup from previous merge

--- tests/Schemas/Anthropic/AnthropicTextHandlerTest.php
+++ tests/Schemas/Anthropic/AnthropicTextHandlerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 use Prism\Prism\Facades\Tool;
 use Prism\Prism\Prism;
-use Prism\Prism\ValueObjects\Media\Image;
+use Prism\Prism\ValueObjects\Messages\Support\Image;
 use Prism\Prism\ValueObjects\Messages\SystemMessage;
 use Prism\Prism\ValueObjects\Messages\UserMessage;
 use Tests\Fixtures\FixtureResponse;
@@ -203,7 +203,7 @@ it('does not remove 0 values from payloads', function (): void {
         ->usingTemperature(0)
         ->asText();
 
-    Http::assertSent(fn (Request $request): \Pest\Expectation => expect($request->data())->toMatchArray([
+    Http::assertSent(fn (Request $request): \Pest\Mixins\Expectation => expect($request->data())->toMatchArray([
         'temperature' => 0,
     ]));
 });
